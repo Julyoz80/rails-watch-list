1686dd3f8a2942d8293417e6369b16fcc6d32186
Julie Boulard <juliefm1380@gmail.com>
2025-05-29 15:18:24 +0200

    create seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_29_103250) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_29_125716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_103250) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "overview"
-    t.string "poster"
+    t.string "poster_url"
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
